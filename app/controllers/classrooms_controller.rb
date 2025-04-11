0ccaa29f1527c6e473667a9f5f9c6fc22cdb777b
Ronesh Ghoshal <roneshghoshal@roneshs-mbp.lan>
2025-04-11 16:35:41 -0400

    Start building new classroom form

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,5 +1,5 @@
 class ClassroomsController < ApplicationController
-  before_action :set_school, only: [ :create ]
+  before_action :set_school, only: [ :create, :new ]
   before_action :set_classroom, only: [ :destroy, :show ]
 
   def create
@@ -10,6 +10,10 @@ class ClassroomsController < ApplicationController
     else
       redirect_to edit_school_path(@school), alert: "Failed to create classroom."
     end
+  end
+
+  def new
+    @classroom = @school.classrooms.build
   end
 
   def destroy
@@ -24,7 +28,7 @@ class ClassroomsController < ApplicationController
   private
 
   def set_school
-    @school = School.find(params[:school_id])
+    @school = current_user.school || School.find(params[:school_id])
   end
 
   def set_classroom
@@ -32,6 +36,6 @@ class ClassroomsController < ApplicationController
   end
 
   def classroom_params
-    params.require(:classroom).permit(:name)
+    params.require(:classroom).permit(:name, :school_year, teachers_attributes: [:email_address, :role], students_attributes: [:first_name, :last_name, :dob, contacts_attributes: [:name, :phone, :email, :relationship]])
   end
 end
