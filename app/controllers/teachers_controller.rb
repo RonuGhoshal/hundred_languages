e68d6fa5fd08f84c201ae408b87a65a21a29e6c7
Ronesh Ghoshal <RonuGhoshal@gmail.com>
2025-04-15 15:00:15 -0400

    Add teachers index

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,24 @@
 class TeachersController < ApplicationController
+  before_action :set_teacher, only: [ :show, :edit ]
+  before_action :set_school, only: [ :index ]
+
+  def index
+    @teachers = @school.teachers
+  end
+
   def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_teacher
     @teacher = Teacher.find(params[:id])
+  end
+
+  def set_school
+    @school = current_user.school
   end
 end
