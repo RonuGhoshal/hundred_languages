c8c6bda38191b919cd71ad8095e24b6e1fcde674
Ronesh Ghoshal <RonuGhoshal@gmail.com>
2025-04-15 10:20:06 -0400

    Working on show student view.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
   resources :classrooms, only: [ :show, :new, :edit, :update ]
   resources :teachers, only: [ :show, :edit, :update ]
+  resources :students, only: [ :show, :edit ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
